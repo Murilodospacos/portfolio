cfd01b76c0aedcb8d51aa2ac1e015c4d9863857d
Murilo Paços <murilopacos@gmail.com>
2022-01-05 09:30:07 -0300

    corrigindo escrita titulo portfolio

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function NavBar() {
   return (
      <header className='navbar-header'>
-      <Link to="/" style={{ textDecoration: 'none' }}><h2 className='navbar-h2'>{`<Portifolio />`}</h2></Link>
+      <Link to="/" style={{ textDecoration: 'none' }}><h2 className='navbar-h2'>{`<Portfólio />`}</h2></Link>
       <nav className='navbar-nav'>
         <Link to="/" style={{ textDecoration: 'none' }}><h3 className='navbar-h3'>Home</h3></Link>
         <Link to="/sobre" style={{ textDecoration: 'none' }}><h3 className='navbar-h3'>Sobre</h3></Link>
